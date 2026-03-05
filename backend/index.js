a9ce07a6377f0b2814c9a39e3bfedbf492c869da
LAKSHYA <1lakshyamalani234@gmail.com>
2026-03-05 06:26:45 +0530

    prepare for deployment

--- backend/index.js
+++ backend/index.js
@@ -183,8 +183,8 @@ mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("✅ MongoDB connected!");
-    httpServer.listen(process.env.PORT, () => {
-      console.log(`✅ Server running on port ${process.env.PORT}`);
+    httpServer.listen(process.env.PORT || 5000, () => {
+      console.log(`✅ Server running on port ${process.env.PORT || 5000}`);
     });
   })
   .catch((err) => {
